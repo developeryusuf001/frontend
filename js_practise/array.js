2407cf90818ba0282f0610c5dfbb38fc6b35c010
developeryusuf001 <mohammadyusufcse001@gmail.com>
2026-04-19 01:33:01 +0530

    printed sum of all the array elements

--- js_practise/array.js
+++ js_practise/array.js
@@ -1,5 +1,8 @@
 let arr = [12, 24, 36, 48, 60, 72, 84];
+let sum = 0;
 
 for(let i = 0; i < arr.length; i++){
     console.log(arr[i]);
+    sum = sum + arr[i];
 }
+console.log("The sum of array elements are: " + sum);
