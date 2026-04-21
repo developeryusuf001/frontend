3d90265523da95f218b092fc8b2ef513f477ad63
developeryusuf001 <mohammadyusufcse001@gmail.com>
2026-04-21 19:32:30 +0530

    add largest number determination logic

---
+++ js_practise/largest.js
@@ -0,0 +1,13 @@
+let a = 91
+let b = 98
+let c = 13
+
+if (a > b && a > c){
+    console.log("a is largest: " + a);
+}
+else if (b > a && b > c){
+    console.log("b is the largest: " + b);
+}
+else{
+    console.log("c is the largest: " + c);
+}
