89344197a1d49b65738ab9762b5b310882a259d6
developeryusuf001 <mohammadyusufcse001@gmail.com>
2026-04-21 19:32:16 +0530

    add array initialization and method examples

---
+++ js_practise/array1.js
@@ -0,0 +1,23 @@
+const myarr = new Array(1,2,3,4,5)
+console.log(myarr[1]);
+
+// Array Methods
+
+// myarr.push(6)
+// myarr.push(7)
+// myarr.push(8)
+// myarr.pop()
+
+// myarr.unshift(9)
+// myarr.unshift(15) // unshift(num) is used to add one or more elements to the beginning of an array.
+// myarr.shift()   // shift() is used to remove the first element of an array.
+
+// console.log(myarr.includes(9))   // include() - returns boolean answer.
+// console.log(myarr.indexOf(19))   // indexOf() - returns -1 when element is not present in the array.
+
+const newarr = myarr.join()
+
+
+console.log(myarr);
+console.log(typeof newarr);
+
