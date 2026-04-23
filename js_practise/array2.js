e72bf439910eca85e19ae972c2a32423c4c4aff7
developeryusuf001 <mohammadyusufcse001@gmail.com>
2026-04-23 23:40:14 +0530

    add: implement array operations and examples in array2.js

---
+++ js_practise/array2.js
@@ -0,0 +1,21 @@
+const marvel_heros = ["thor", "spiderman", "ironman"]
+const dc = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][2]);
+
+// const heros = marvel_heros.concat(dc)
+// console.log(heros);
+
+const all = [...marvel_heros, ...dc]   // used spread operator here...
+console.log(all)
+
+const arr2 = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const arr21 = arr2.flat(Infinity)   
+console.log(arr21);
+
+
+console.log(Array.isArray("Yusuf"))   // returns false because it checks that its not an array.
+console.log(Array.from("Yusuf"))   // from converts the string into individual letter array.
