fcbdf71c55fc7888f83d53ab36c41635acf5c6b3
developeryusuf001 <mohammadyusufcse001@gmail.com>
2026-04-24 00:45:05 +0530

    add: create instauser object with properties in objects2.js

---
+++ js_practise/objects2.js
@@ -0,0 +1,30 @@
+ const instauser = new Object()
+const instauser = {}
+
+instauser.id = "t72t7y2378y248y2"
+instauser.name = "Mohammad Yusuf"
+instauser.isLoggedIn = false
+// console.log(instauser)
+
+
+// const regularUser = {
+//     email: "hello@gmail.com",
+//     fullname: {           // nested objects created.
+//         usefulname: {
+//             firstname: "Mohammad",
+//             lastname: "Yusuf"
+//         }
+//     }
+// }
+// console.log(regularUser.fullname);
+
+// const obj1 = {1: "String", 2: "h"}
+// const obj2 = {3: "hello", 4: "56"}
+
+// con
+// st obj3 = {obj1, obj2}
+// const obj4 = Object.assign({},obj1, obj2)
+// const obj4 = {...obj1, ...obj2}
+// console.log(obj4);
+
+console.log(Object.keys(instauser));
