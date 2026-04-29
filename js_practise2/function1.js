8a21daf44225f5618efcfe09bc62a8ee61ad9f68
developeryusuf001 <mohammadyusufcse001@gmail.com>
2026-04-29 23:38:26 +0530

    refactor: comment out loginuserMessage function and its usage

--- js_practise2/function1.js
+++ js_practise2/function1.js
@@ -1,9 +1,16 @@
-function loginuserMessage(username = "rai"){
-    if(!username){
-        console.log("PLease enter a username");
-        return
-    }
-    return `${username} just logged in`
-}
+// function loginuserMessage(username = "rai"){
+//     if(!username){
+//         console.log("PLease enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
 
-console.log(loginuserMessage("yusuf"));
+// console.log(loginuserMessage("yusuf"));
+
+
+function calculateCartPrice(...num1){   // ... is called as spread operator and also as rest operator, it depends on its use case.
+    return num1
+ 
+}
+console.log(calculateCartPrice(200, 400, 500));
