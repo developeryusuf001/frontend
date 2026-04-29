c2479c1498e03ec3166d578993d2b00710080dd8
developeryusuf001 <mohammadyusufcse001@gmail.com>
2026-04-30 00:20:45 +0530

    refactor: comment out unused variables and functions in scopes.js

--- js_practise2/scopes.js
+++ js_practise2/scopes.js
@@ -1,12 +1,36 @@
-let a = 1000
+// let a = 1000
+
+// if(true){
+//     let a = 2
+//     const b = 4
+//     console.log("INNER: ", a);
+// }
+
+// console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+// function one(){
+//     const username = "yusuf"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+
+//     two()
+// }
+// one() 
 
 if(true){
-    let a = 2
-    const b = 4
-    console.log("INNER: ", a);
+    const username = "yusuf";
+    if(username === "yusuf"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
 }
+// console.log(username);
 
-console.log(a);
-// console.log(b);
-// console.log(c);
-
+// ****************************** intresting ***********************
