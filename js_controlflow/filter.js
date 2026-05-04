100379f5fc941681201c486e64764dc21765b262
yusuf-in <mohammadyusuf.work@gmail.com>
2026-05-05 00:25:42 +0530

    add: create filter.js with examples of the filter method for arrays

---
+++ js_controlflow/filter.js
@@ -0,0 +1,12 @@
+const myNum = [1,2,3,4,6,7,8,9,2,3,5]
+
+// const newWord = myNum.filter((num) => num > 4)  
+// console.log(newWord);
+
+// const newWord = myNum.filter((num) => {
+//     return num > 4
+// })  
+// console.log(newWord);
+
+
+ 
